93347c9ddda170c61c4b5a21a135b169d9fe51ac
Dennis Lee <dennis.l@digipen.edu>
2018-05-11 17:10:40 -0700

    Simple example

---
+++ app.js
@@ -0,0 +1,6 @@
+const myHeading = document.getElementById("myHeading");
+//const myHeading = document.getElementById("myButton");
+
+myHeading.addEventListener("click", () => {
+    myHeading.style.color = 'red';
+});
