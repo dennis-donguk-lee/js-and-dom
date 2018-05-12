74cae238d39af1aec193023f5e6128a8ef847a15
Dennis Lee <dennis.l@digipen.edu>
2018-05-11 21:53:31 -0700

    Appending nodes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const descriptionInput = document.querySelector("input");
 const descriptionP = document.querySelector("p.description");
 const descriptionButton = document.querySelector("button.description");
 const addItemInput = document.querySelector("input.addItemInput");
-const addItemButton = document.querySelector("input.addItemButton");
+const addItemButton = document.querySelector("button.addItemButton");
 
 toggleList.addEventListener("click", () => {
     if (listDiv.style.display == "none") {
@@ -18,9 +18,13 @@ toggleList.addEventListener("click", () => {
 
 descriptionButton.addEventListener("click", () => {
     descriptionP.innerHTML = descriptionInput.value + ':';
+    descriptionInput.value = "";
 });
 
 addItemButton.addEventListener("click", () => {
+    let ul = document.getElementsByTagName("ul")[0];
     let li = document.createElement("li");
     li.textContent = addItemInput.value;
+    ul.appendChild(li);
+    addItemInput.value = "";
 });
