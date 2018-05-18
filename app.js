4ada4a4274bfb40e58be922859b272473f1cf2f2
Dennis Lee <dennis.l@digipen.edu>
2018-05-17 22:58:01 -0700

    Getting all children of a node with children

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ const descriptionButton = document.querySelector("button.description");
 const listUl = document.querySelector("ul");
 const addItemInput = document.querySelector("input.addItemInput");
 const addItemButton = document.querySelector("button.addItemButton");
+const lis = listUl.children;
+
+function attachListItemButtons(li) {
+    let up = document.createElement("button");
+    up.className = "up";
+    up.textContent = "Up";
+    li.appendChild(up);
+
+    let down = document.createElement("button");
+    down.className = "down";
+    down.textContent = "Down";
+    li.appendChild(down);
+
+    let remove = document.createElement("button");
+    remove.className = "remove";
+    remove.textContent = "Remove";
+    li.appendChild(remove);
+}
+
+for (let i = 0; i < lis.length; ++i) {
+    attachListItemButtons(lis[i]);
+}
 
 listUl.addEventListener("click", (event) => {
     if (event.target.tagName === "BUTTON" /* && event.target.parentNode.tagName === "LI" */) {
@@ -47,6 +69,7 @@ addItemButton.addEventListener("click", () => {
     let ul = document.getElementsByTagName("ul")[0];
     let li = document.createElement("li");
     li.textContent = addItemInput.value;
+    attachListItemButtons(li);
     ul.appendChild(li);
     addItemInput.value = "";
 });
